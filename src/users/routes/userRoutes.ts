bc051d23e0c27187db74c7fda86c03050287b066
Jesús Ramos <jesusramosdev@gmail.com>
2019-11-20 17:29:19 +0000

    Añadido endpoint ejemplo userRoutes

---
+++ src/users/routes/userRoutes.ts
@@ -0,0 +1,9 @@
+import { Router, Response, Request } from 'express';
+const router:Router = Router();
+import { IUserModel } from '../model/userModel';
+
+router.get('/', (req:Request, res:Response) => {
+  
+});
+
+export default router;
