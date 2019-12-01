3955a2b8dd55f3c33f8bd4568eb4eb84a368a226
Jesús Ramos <jesusramosdev@gmail.com>
2019-12-01 20:13:55 +0000

    Corregido error método validate

--- src/utils/Validator.ts
+++ src/utils/Validator.ts
@@ -1,40 +1,40 @@
 export default class Validator {
-  static validate(validators: []): boolean {
+  static validate(input: string, validators: []): boolean {
     validators.forEach((validator: Function) => {
-      if (!validator()) {
+      if (!validator(input)) {
         return false;
       }
     });
     return true;
   }
 
-  static isNumeric(arg: string): boolean {
+  static isNumeric(input: string): boolean {
     const regex: RegExp = /^\d+$/;
-    if (!arg.match(regex)) {
+    if (!input.match(regex)) {
       return false;
     }
     return true;
   }
 
-  static hasSpecialChars(arg: string): boolean {
+  static hasSpecialChars(input: string): boolean {
     const regex: RegExp = /`|~|!|@|#|\$|%|\^|&|\*|\(|\)|\+|=|\[|\{|\]|\}|\||\\|'|<|,|\.|>|\?|\/|""|;|:|\s/g;
-    if (!arg.match(regex)) {
+    if (!input.match(regex)) {
       return false;
     }
     return true;
   }
 
-  static isEmail(arg: string): boolean {
+  static isEmail(input: string): boolean {
     const regex: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!arg.match(regex)) {
+    if (!input.match(regex)) {
       return false;
     }
     return true;
   }
 
-  static isValidDate(arg: string): boolean {
+  static isValidDate(input: string): boolean {
     const regex: RegExp = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
-    if (!arg.match(regex)) {
+    if (!input.match(regex)) {
       return false;
     }
     return true;
