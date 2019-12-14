38390233d9f208f30dbbbcc06238948fc964586e
Jesús Ramos <jesusramosdev@gmail.com>
2019-12-14 16:51:17 +0000

    Prueba middleware jwt

--- src/middlewares/verifyJWT.ts
+++ src/middlewares/verifyJWT.ts
@@ -1,21 +1,20 @@
-// import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 // import jwt from 'jsonwebtoken';
 // import config from '../config/config';
 
-// /**
-//  * Verifica que un usuario está logueado y si dispone de un token
-//  * válido al realizar la petición
-//  * @param req
-//  * @param res
-//  * @param next
-//  */
-// export const verifyJWT = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ): void => {
-//   const token: string = req.headers.authorization.split(' ')[1];
-//   let jwtPayload;
+/**
+ * Verifica que un usuario está logueado y si dispone de un token
+ * válido al realizar la petición
+ * @param req
+ * @param res
+ * @param next
+ */
+export const verifyJWT = (req: Request, res: Response): Response => {
+  if (!req.headers.authorization) return res.status(401);
+};
+
+// const token: string = req.headers.authorization.split(' ')[1];
+// let jwtPayload;
 
 //   //Try to validate the token and get data
 //   try {
