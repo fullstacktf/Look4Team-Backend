ce0aebfd1118f285d81f946fb92a75d08b84fe14
Jesús Ramos <jesusramosdev@gmail.com>
2019-12-15 15:29:52 +0000

    Corregido error db

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export default {
   jwtSecret: 'supersecret',
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB || 'mongodb://127.0.0.1:27017/look4team'
+  db: process.env.MONGODB || 'mongodb://db:27017/look4team'
 };
